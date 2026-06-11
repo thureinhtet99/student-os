4d6191a055f667d22cec7ff6513aeb3a490db01b
thureinhtet99 <ignite25399@gmail.com>
2026-06-12 03:17:10 +0700

    re-structure mobile repo

---
+++ mobile/app/message/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function MessageIndex() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl font-bold">Messages</Text>
+      <Text className="text-gray-500">Communicate with teachers and students.</Text>
+    </View>
+  );
+}
