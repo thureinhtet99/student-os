2558065be7d786523dd205264a917ae1113469c1
thureinhtet99 <ignite25399@gmail.com>
2026-07-03 21:44:03 +0700

    refactor: update Makefile commands and structure for clarity; add flow diagram and enhance schema definitions

--- server/src/common/utils/auth.ts
+++ server/src/common/utils/auth.ts
@@ -50,11 +50,11 @@ export const auth = betterAuth({
         required: false,
         input: false,
       },
-      createdBy: {
-        type: 'string',
-        required: false,
-        input: false,
-      },
+      // createdBy: {
+      //   type: 'string',
+      //   required: false,
+      //   input: false,
+      // },
       lastLoginAt: {
         type: 'date',
         required: false,
